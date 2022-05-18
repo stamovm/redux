b61a26170cadf6110ecdb5802b7bf14fcf94605b
stamovm <m.stamov@yahoo.com>
2022-05-17 23:39:47 -0700

    Added redux to the project. Created store and reducer function.

--- project1/src/App.js
+++ project1/src/App.js
@@ -3,10 +3,24 @@ import CartContainer from './components/CartContainer'
 
 import cartItems from './cart-items'
 
+import { createStore } from 'redux'
+
+const initStore = {
+  count: 3,
+}
+
+function reducer(state, action) {
+  console.log('reducer', state, action)
+  return state
+}
+
+const store = createStore(reducer, initStore)
+console.log(store.getState())
+
 function App() {
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   )
